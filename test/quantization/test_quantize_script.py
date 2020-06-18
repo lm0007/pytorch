806bcd6fa123742563d35f2436000d51666dc8d1
vasiliy <vasiliy@fb.com>
2020-06-18 11:34:16 -0700

    quantized elu: graph mode handling
    Summary:
    
    Adds graph mode handling for quantized elu.
    
    Test Plan:
    
    ```
    python test/test_quantization.py TestQuantizeScriptPTSQOps.test_elu
    ```
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: a20c99b74d9ac9d5aefe515be6f8fce1de555526
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/40111

--- test/quantization/test_quantize_script.py
+++ test/quantization/test_quantize_script.py
@@ -2089,6 +2089,14 @@ class TestQuantizeScriptPTSQOps(QuantizationTestCase):
             FileCheck().check_not("aten::hardswish") \
                        .run(m.graph)
 
+    def test_elu(self):
+        data = [(torch.rand((1, 2, 5, 5), dtype=torch.float), torch.randint(0, 1, (1,), dtype=torch.long)) for _ in range(2)]
+        elu = torch.nn.ELU()
+        for tracing in [True, False]:
+            m = self.checkGraphModeOp(elu, data, "quantized::elu", tracing)
+            FileCheck().check_not("aten::elu") \
+                       .run(m.graph)
+
     def test_layer_norm(self):
         data = [(torch.rand((1, 2, 5, 5), dtype=torch.float), torch.randint(0, 1, (1,), dtype=torch.long)) for _ in range(2)]
         layer_norm = torch.nn.LayerNorm([2, 5, 5])
